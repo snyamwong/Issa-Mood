ea2b74700e102dea9a75cd3b2309bd0d35fe9646
Tin Wong <wongt1@wit.edu>
2019-06-11 17:49:18 -0400

    parse_lyrics returns a dictionary of emotions for lyrics
    timing improvements needed, as well as general code cleanup

--- nlp/parse_lyrics.py
+++ nlp/parse_lyrics.py
@@ -2,17 +2,24 @@
 Python Script that cleans up song lyrics
 
 # TODO: needs to clean out whatever Genius web scrapping returns
-# TODO: Singuarlize still needs some work
+# TODO: improve lemmatize (analyzing the pos)
 # TODO: 2-grams
+# TODO: improve timing
+# TODO: refactor code asap
+# TODO: documentation oof
 """
 
+import time
 import nltk
 import string
 import inflection
 import inflect
 
+from collections import defaultdict
+from lexicon import Lexicon
+from inflection import singularize
+from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
 from textblob import TextBlob
 
 """
@@ -21,12 +28,6 @@ nltk.download('punkt')
 nltk.download('brown')
 nltk.download('wordnet')
 """
-
-stop_words = set(stopwords.words('english'))
-stop_words.add('ai') # ain't
-stop_words.add('wa') # wasn't
-
-p = inflect.engine()
 
 def readSongLyrics(songpath):
 
@@ -40,6 +41,12 @@ def readSongLyrics(songpath):
 
 def getFilteredText(blob):
 
+    stop_words = set(stopwords.words('english'))
+    stop_words.add('ai') # ain't
+    stop_words.add('wa') # wasn't
+    p = inflect.engine()
+    wnl = WordNetLemmatizer()
+
     result = []
 
     for sentence in blob:
@@ -48,14 +55,27 @@ def getFilteredText(blob):
         # cleaning the words
         words = [w.lower().translate(str.maketrans('', '', string.punctuation)) for w in text.words]
 
-        # filter out words
-        filtered_text = [w for w in words if not w in stop_words]
-
-        filtered_text = ' '.join(str(word) if(p.singular_noun(str(word))) else inflection.singularize(str(word)) for word in filtered_text if len(word) > 0)
+        # iT's pyThoNic
+        filtered_text = ' '.join(wnl.lemmatize(str(w), pos='v') if(p.singular_noun(str(w))) else singularize(str(w)) for w in words if not w in stop_words)
 
         result.append(filtered_text)
 
     return result
+
+def getLyricsEmotions(lyrics):
+
+    emotion_dict = defaultdict(int)
+
+    lexicon = Lexicon()
+
+    for sentence in lyrics:
+        for word in sentence.split():
+            emotions = lexicon.wordAssociation(word)
+
+            for emo in emotions:
+                emotion_dict[emo] += 1
+
+    return emotion_dict
 
 if __name__ == '__main__':
 
@@ -63,9 +83,17 @@ if __name__ == '__main__':
 
     lyrics = readSongLyrics(songpath)
 
+    t0 = time.time()
+
     filtered_lyrics = getFilteredText(lyrics)
 
-    print(filtered_lyrics)
+    t1 = time.time()
+
+    print('timing for filtering lyrics:', t1 - t0)
+
+    emotions = getLyricsEmotions(filtered_lyrics)
+
+    print(emotions)
 
     """
     two_gram = filtered_sentence.ngrams(2)
