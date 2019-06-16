cd3f03e3b9e4d2425e50ac1a18753c452c05e975
Tin Wong <wongt1@wit.edu>
2019-06-16 01:52:33 -0400

    cleaned up parse lyrics
    also improved timing and lemmatize a little bit

--- nlp/parse_lyrics.py
+++ nlp/parse_lyrics.py
@@ -2,35 +2,32 @@
 Python Script that cleans up song lyrics
 
 # TODO: needs to clean out whatever Genius web scrapping returns
-# TODO: improve lemmatize (analyzing the pos)
 # TODO: 2-grams
 # TODO: improve timing
-# TODO: refactor code asap
 # TODO: documentation oof
 # TODO: Text reduction to improve timing(?)
-"""
 
-import time
-import statistics
-import nltk
-import string
-import inflection
-import inflect
-
-from collections import defaultdict
-from lexicon import Lexicon
-from inflection import singularize
-from nltk.stem import WordNetLemmatizer
-from nltk.corpus import stopwords
-from textblob import TextBlob
-
-"""
+things you need to download
 nltk.download('stopwords')
 nltk.download('punkt')
 nltk.download('brown')
 nltk.download('wordnet')
 """
 
+import threading
+import time
+import statistics
+import nltk
+import string
+
+from collections import defaultdict
+from lexicon import Lexicon
+from nltk.corpus import stopwords
+from textblob import TextBlob
+
+"""
+
+"""
 def readSongLyrics(songpath):
 
     # what if i make a TextBlob here then just append to it?
@@ -45,40 +42,57 @@ def readSongLyrics(songpath):
 
     return lyrics
 
-def getFilteredText(blob):
+"""
+
+"""
+def filterLyrics(blob):
 
     stop_words = set(stopwords.words('english'))
+    # words left over from lemmatize
     stop_words.add('ai') # ain't
     stop_words.add('wa') # wasn't
     stop_words.add('ca') # can't
-    p = inflect.engine()
-    wnl = WordNetLemmatizer()
+    stop_words.add('wo') # won't
+    stop_words.add('m')
 
-    result = []
+    # genius tags
+    stop_words.add('intro')
+    stop_words.add('verse')
+    stop_words.add('hook')
+    stop_words.add('chorus')
+    stop_words.add('prechorus')
+
+    filtered_words = []
 
     for sentence in blob:
         text = TextBlob(sentence)
 
-        # cleaning the words
-        words = [w.lower().translate(str.maketrans('', '', string.punctuation)) for w in text.words]
+        words = []
+        for w in text.words:
+            w = w.lemmatize('n')
+            w = w.lower()
+            w = w.translate(str.maketrans('', '', string.punctuation))
 
-        # iT's pyThoNic
-        filtered_text = ' '.join(wnl.lemmatize(str(w), pos='v') if(p.singular_noun(str(w))) else singularize(str(w)) for w in words if not w in stop_words)
+            if(w not in stop_words and not w.isdigit()):
+                words.append(w)
 
-        result.append(filtered_text)
+        if len(words) > 0:
+            filtered_words.append(' '.join(w for w in words))
 
-    #print('filtered_text', result)
+    #print('filtered_words', filtered_words)
 
-    return result
+    return filtered_words
 
+"""
+
+"""
 def getLyricsEmotions(lyrics):
 
     emotion_dict = defaultdict(int)
 
     lexicon = Lexicon()
 
-    timings = []
-
+    # group by key
     for sentence in lyrics:
         t0 = time.time()
         for word in sentence.split():
@@ -87,35 +101,35 @@ def getLyricsEmotions(lyrics):
             for emo in emotions:
                 emotion_dict[emo] += 1
 
-        t1 = time.time()
-        timings.append(t1-t0)
-
-    #print(timings)
-    #print(statistics.mean(timings))
+    # merge
+    # emotion_count = defaultdict(int)
 
     return emotion_dict
 
 if __name__ == '__main__':
 
-    songpath = '../song/grief.txt'
+    songpath = '../song/cold.txt'
     t0 = time.time()
     lyrics = readSongLyrics(songpath)
     t1 = time.time()
-
     print('timing for reading lyrics:', t1 - t0)
 
     t0 = time.time()
-    filtered_lyrics = getFilteredText(lyrics)
+    filtered_lyrics = filterLyrics(lyrics)
     t1 = time.time()
-
     print('timing for filtering lyrics:', t1 - t0)
 
     t0 = time.time()
     emotions = getLyricsEmotions(filtered_lyrics)
     t1 = time.time()
-
     print('timing for getting emotions', t1 - t0)
     print(emotions)
+
+    """
+    thread = threading.Thread(target=getLyricsEmotions, args=(filtered_lyrics))
+    thread.start()
+    thread.join()
+    """
 
     """
     two_gram = filtered_sentence.ngrams(2)
