09965a6abdff1b23f2daeb34aed8a4f267af8cc5
Tin Wong <wongt1@wit.edu>
2019-07-28 17:40:01 -0400

    mark in results page reflect with plutchik
    different colors for each emotions, displays the chart as well

--- flaskr/results.py
+++ flaskr/results.py
@@ -1,63 +1,96 @@
+"""
+"""
 import sys
-from lyrics import Lyrics
-from genius import Genius
-from db import Database
 import pickle
+from lyrics import Lyrics
+from db import Database
 
-def generate_results_data(song_string,artist_string,genius,results):
-	lyrics = Lyrics()
-	database = Database()
-	
-	song_string = genius.song
-	artist_string = genius.artist
-    #if data's song and artist don't exist in the database, do all the normal heavy loading and then store it in the database
-	if database.data_exists(song_string,artist_string)==False:
-	    album_img_string = genius.album_img
-	    #time0 = time.time()
-	    filtered_lyrics = lyrics.filter_lyrics(results)
-	    emotions = lyrics.get_lyrics_emotions(filtered_lyrics)
-	    agg_emotions = lyrics.get_agg_emotions(filtered_lyrics)
-	    #this takes in all of the information that is passed to the tenplate normally and stores it for later use
-	    database.store_data(song_string,artist_string,album_img_string,results,emotions,agg_emotions)
-	#if data exists in database, retrieve it and then store it's normal variables
-	else:
-	    database_data = database.retrieve_data(song_string,artist_string)
-	    album_img_string=database_data[2]
-	    results=database_data[3]
-	    emotions=pickle.loads(database_data[4])
-	    agg_emotions=pickle.loads(database_data[5])
 
-	results = highlight_emotion_sentences(emotions,results)
+def generate_results_data(song_string, artist_string, genius, results):
+    """
+    """
 
-	results_data = [results,song_string,artist_string,album_img_string,emotions,agg_emotions]
-	return results_data
+    lyrics = Lyrics()
+    database = Database()
+
+    song_string = genius.song
+    artist_string = genius.artist
+    # if data's song and artist doesn't exist in db, do normal loading and then store in db
+    if not database.data_exists(song_string, artist_string):
+        album_img_string = genius.album_img
+        #time0 = time.time()
+        filtered_lyrics = lyrics.filter_lyrics(results)
+        emotions = lyrics.get_lyrics_emotions(filtered_lyrics)
+        agg_emotions = lyrics.get_agg_emotions(filtered_lyrics)
+        # takes in all of the info that is passed to the template and stores it for later use
+        database.store_data(song_string, artist_string,
+                            album_img_string, results, emotions, agg_emotions)
+    # if data exists in database, retrieve it and then store it's normal variables
+    else:
+        database_data = database.retrieve_data(song_string, artist_string)
+        album_img_string = database_data[2]
+        results = database_data[3]
+        emotions = pickle.loads(database_data[4])
+        agg_emotions = pickle.loads(database_data[5])
+
+    results = highlight_emotion_sentences(emotions, results)
+
+    results_data = [results, song_string, artist_string,
+                    album_img_string, emotions, agg_emotions]
+
+    return results_data
 
 
 def highlight_emotion_sentences(emotions_list, lyrics):
-    highlight_line = False
+    """
+    """
+
     # list of emotions that will be iterated through
-    emotion = ["anticipation", "joy", "trust", "sadness",
-               "fear", "surprise", "anger", "disgust"]
+    emotions = {"anticipation": "#FF7F00",
+                "joy": "#FFEA52",
+                "trust": "#52FF52",
+                "sadness": "#0000CA",
+                "fear": "#009800",
+                "surprise": "#008BE2",
+                "anger": "#D60000",
+                "disgust": "#E000E0"}
+
     # 8 versions of the lyrics will be stored in this, all marked up for their respective emotion
     lyrics_list = []
     lyrics_temp = lyrics
+    highlight_line = False
+
     # run the loop once for each emotion
-    for emotion in emotion:
-        # for each clump of data, look at the emotions in the counter and see if it is present for that clump, if it is, set this line to be highlighted
+    for emotion, color in emotions.items():
+        # for each clump of data, look at emotions in counter
+        # if it is present for that clump set this line to be highlighted
         for clump in emotions_list:
             for counter in clump:
                 if emotion in counter:
                     highlight_line = True
 
-            if highlight_line == True:
+            if highlight_line:
                 # sets the clumps current line of the song to be highlighted
                 string_to_highlight = clump.original
-                # if the mark for this string isn't a duplicate, add mark tags to that line of the song
+                # if the mark for this string isn't a duplicate, add mark tags to that line
                 if lyrics_temp.find("<mark>{}</mark>".format(string_to_highlight)) == -1:
+                    if emotion in ["anticipation", "sadness", "fear", "anger", "disgust"]:
+                        mark_style = "style=\"background-color: {}; color: white\"".format(
+                            color)
+                    else:
+                        mark_style = "style=\"background-color: {}\"".format(
+                            color)
+
+                    mark_tag = "<mark {}>{}</mark>".format(
+                        mark_style, string_to_highlight)
+
+                    print(mark_tag, file=sys.stderr)
+
                     lyrics_temp = lyrics_temp.replace(
-                        string_to_highlight, "<mark>{}</mark>".format(string_to_highlight))
-                # reset back to false for the next iteration of the loop
-                highlight_line = False
+                        string_to_highlight, mark_tag)
+
+                    # reset back to false for the next iteration of the loop
+                    highlight_line = False
 
         # after it is done adding marks, add to the list
         lyrics_list.append(lyrics_temp)
